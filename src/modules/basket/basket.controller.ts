466e20365a9bf6242d6e4c22760176a1e3a16fe1
hossein <hosseinfalah2021@gmail.com>
2025-02-22 12:54:21 +0330

    feat: Remove discount from user basket with validation

--- src/modules/basket/basket.controller.ts
+++ src/modules/basket/basket.controller.ts
@@ -1,9 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Post } from '@nestjs/common';
 import { BasketService } from './basket.service';
 import { ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { AuthDecorator } from 'src/common/decorators/auth.decorator';
 import { SwaggerConsmes } from 'src/common/enums/swagger.consumes.enum';
-import { BasketDto } from './dto/basket.dto';
+import { BasketDto, DiscountBasketDto } from './dto/basket.dto';
 
 @Controller('basket')
 @ApiTags("Basket")
@@ -15,5 +15,11 @@ export class BasketController {
   @ApiConsumes(SwaggerConsmes.UrlEncoded, SwaggerConsmes.Json)
   addToBasket(@Body() basketDto:BasketDto) {
     return this.basketService.addToBasket(basketDto)
+  }
+
+  @Delete('/discount')
+  @ApiConsumes(SwaggerConsmes.UrlEncoded, SwaggerConsmes.Json)
+  removeDiscountFormBasket(discountBasketDto:DiscountBasketDto) {
+    return this.basketService.removeDiscountFormBasket(discountBasketDto);
   }
 }
--- src/modules/basket/basket.service.ts
+++ src/modules/basket/basket.service.ts
@@ -2,18 +2,20 @@ import { Request } from 'express';
 import { Repository } from 'typeorm';
 import { REQUEST } from '@nestjs/core';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ConflictException, Inject, Injectable, Scope } from '@nestjs/common';
-import { BasketDto } from './dto/basket.dto';
+import { ConflictException, Inject, Injectable, NotFoundException, Scope } from '@nestjs/common';
+import { BasketDto, DiscountBasketDto } from './dto/basket.dto';
 import { BasketEntity } from './entities/basket.entity';
 import { CourseService } from '../course/services/course.service';
-import { BasketMessage } from 'src/common/enums/message.enum';
+import { BasketMessage, DiscountMessage } from 'src/common/enums/message.enum';
+import { DiscountService } from '../discount/discount.service';
 
 @Injectable({ scope: Scope.REQUEST })
 export class BasketService {
   constructor(
     @InjectRepository(BasketEntity) private basketRepository:Repository<BasketEntity>,
     @Inject(REQUEST) private request:Request,
-    private courseService:CourseService
+    private courseService:CourseService,
+    private discountService:DiscountService
   ) {}
   
   async addToBasket(basketDto: BasketDto) {
@@ -38,6 +40,25 @@ export class BasketService {
 
     return {
       message: BasketMessage.AddToBasket
+    }
+  }
+
+  async removeDiscountFormBasket(discountBasketDto:DiscountBasketDto) {
+    const { id: userId } = this.request.user;
+    const { code } = discountBasketDto;
+    const discount = await this.discountService.findOneByCode(code);
+    const basketDiscount = await this.basketRepository.findOne({
+      where: {
+        discountId: discount.id
+      }
+    });
+
+    if (!basketDiscount) throw new NotFoundException(DiscountMessage.NotFound);
+
+    await this.basketRepository.delete({ discountId: discount.id, userId });
+
+    return {
+      message: DiscountMessage.Removed
     }
   }
 }
